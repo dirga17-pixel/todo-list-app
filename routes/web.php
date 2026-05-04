181deb21fe7d3fb053be090598df99747d1a8825
Dirga <dirga2003317@gmail.com>
2026-05-05 05:01:00 +0800

    Fix routes/web.php formatting and indentation

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\TaskController;
-    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 
 Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
-    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
